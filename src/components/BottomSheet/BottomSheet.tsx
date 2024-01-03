16ec8fd014ffac1f04e20ba7bb562efbb33534f5
Carlos Gabriel Godri <carlos.godri@compasso.com.br>
2024-01-03 11:57:42 -0300

    chore: add opacity and close in out

--- src/components/BottomSheet/BottomSheet.tsx
+++ src/components/BottomSheet/BottomSheet.tsx
@@ -1,73 +1,80 @@
-import React from 'react';
-import { View, StyleSheet, Dimensions, Text } from 'react-native';
-import {MaterialCommunityIcons} from '@expo/vector-icons';
-import {Gesture, GestureDetector} from 'react-native-gesture-handler';
-import Animated, { 
-  useSharedValue, 
-  withSpring, 
-  withTiming, 
-  runOnJS, 
+import React from "react";
+import { View, StyleSheet, Dimensions, Text, Pressable } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, {
+  useSharedValue,
+  withSpring,
+  withTiming,
+  runOnJS,
   useAnimatedStyle,
   SlideInDown,
-  SlideOutDown
-} from 'react-native-reanimated';
+  SlideOutDown,
+  FadeIn,
+  FadeOut,
+} from "react-native-reanimated";
 
 const DIMENSION = Dimensions.get("window");
 export const BOTTOM_SHEET_HEIGHT = 220;
 export const SHEET_OVER_DRAG = 20;
 
 type Props = {
-  onClose: ()=>void;
-  visible:boolean;
-}
+  onClose: () => void;
+  visible: boolean;
+};
 
-export function BottomSheet({onClose, visible}:Props) {
-
+export function BottomSheet({ onClose, visible }: Props) {
   const offset = useSharedValue(0);
 
-  const closeBottomSheet = ()=>{
+  const closeBottomSheet = () => {
     offset.value = 0;
     onClose();
-  }
+  };
 
-  const pan = Gesture.Pan().onChange(( event )=>{
-    const offsetDelta = event.changeY + offset.value;
-    const clamp = Math.max(-SHEET_OVER_DRAG, offsetDelta);
+  const pan = Gesture.Pan()
+    .onChange((event) => {
+      const offsetDelta = event.changeY + offset.value;
+      const clamp = Math.max(-SHEET_OVER_DRAG, offsetDelta);
 
-    offset.value = offsetDelta > 0 ? offsetDelta : withSpring(clamp);
-  }).onFinalize(()=>{
-    if(offset.value < BOTTOM_SHEET_HEIGHT / 3){
-      offset.value = withSpring(0);
-    }else{
-      offset.value =  withTiming(BOTTOM_SHEET_HEIGHT, {}, ()=>{
-        runOnJS(closeBottomSheet)()
-      });
-    }
-  })
+      offset.value = offsetDelta > 0 ? offsetDelta : withSpring(clamp);
+    })
+    .onFinalize(() => {
+      if (offset.value < BOTTOM_SHEET_HEIGHT / 3) {
+        offset.value = withSpring(0);
+      } else {
+        offset.value = withTiming(BOTTOM_SHEET_HEIGHT, {}, () => {
+          runOnJS(closeBottomSheet)();
+        });
+      }
+    });
 
-  const translateY = useAnimatedStyle(()=> ({
-    transform: [{translateY:offset.value}]
-  }))
+  const translateY = useAnimatedStyle(() => ({
+    transform: [{ translateY: offset.value }],
+  }));
 
-  if(!visible){
+  if (!visible) {
     return null;
   }
 
   return (
-    <GestureDetector gesture={pan}>
-    <Animated.View 
-    entering={SlideInDown.springify().damping(15)}
-    exiting={SlideOutDown}
-    style={[styles.container, translateY]}>
-      <MaterialCommunityIcons
-      name="drag-horizontal"
-      color="#999"
-      size={24}
-      style={styles.icon}
-      />
-      <Text style={styles.title}>Opcao</Text>
+    <Animated.View entering={FadeIn} exiting={FadeOut} style={styles.out}>
+      <Pressable style={{flex:1}} onPress={closeBottomSheet} />
+      <GestureDetector gesture={pan}>
+        <Animated.View
+          entering={SlideInDown.springify().damping(15)}
+          exiting={SlideOutDown}
+          style={[styles.container, translateY]}
+        >
+          <MaterialCommunityIcons
+            name="drag-horizontal"
+            color="#999"
+            size={24}
+            style={styles.icon}
+          />
+          <Text style={styles.title}>Opcao</Text>
+        </Animated.View>
+      </GestureDetector>
     </Animated.View>
-    </GestureDetector>
   );
 }
 
@@ -75,18 +82,24 @@ const styles = StyleSheet.create({
   container: {
     width: DIMENSION.width,
     height: BOTTOM_SHEET_HEIGHT,
-    backgroundColor: '#1e1f23',
-    position: 'absolute',
+    backgroundColor: "#1e1f23",
+    position: "absolute",
     bottom: 0,
   },
-  title:{
+  title: {
     color: "#fff",
     fontSize: 18,
     fontWeight: "bold",
-    margin: 24
+    margin: 24,
   },
-  icon:{
+  out: {
+    width: DIMENSION.width,
+    height: DIMENSION.height,
+    position: "absolute",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+  },
+  icon: {
     marginTop: 16,
-    alignSelf: 'center'
-  }
+    alignSelf: "center",
+  },
 });
